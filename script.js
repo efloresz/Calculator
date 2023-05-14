9562324db31cf1cc25b5c271b61f336e06a895e7
Emily Flores <e_floresrodriguez@massbay.edu>
2023-05-14 19:32:35 -0400

    calculator commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+// listen for all keypresses 
+keys.addEventListener('click', e => {
+  if(e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displaydNum = display.textContent
+  }
+})
+
+// determines type of key clicked
+const key = e.target
+const action = key.dataset.action
+
+if (!action){
+  if (displayedNum === '0'){
+    display.textContent = keyContent
+  }
+}
+
+if (!action){
+  if (displayedNum === '0'){
+    display.textContent = keyContent
+  } else {
+    display.textContent = displayedNum + keyContent
+  }
+}
+
+if(!action) {
+  console.log('number key')
+}
+
+if(
+  action === 'add' ||
+  action === 'subtract' ||
+  action === 'multiply' ||
+  action === 'divide'
+) {
+  console.log('operator key')
+}
+
+if(action === 'decimal'){
+  console.log('decimal key')
+}
+
+if(action === 'clear'){
+  console.log('clear key')
+}
+
+if(action === 'calculate'){
+  console.log('equal key')
+}
